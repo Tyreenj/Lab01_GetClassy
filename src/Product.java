057aa72bddb840da87f1a1e09e4d6343281edd87
noahj <tyreenj@mail.uc.edu>
2025-09-05 22:18:55 -0400

    Product Commit

---
+++ src/Product.java
@@ -0,0 +1,38 @@
+public class Product {
+    private String name;
+    private String description;
+    private String productID;
+    private double cost;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getProductID() {
+        return productID;
+    }
+
+    public void setProductID(String productID) {
+        this.productID = productID;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+}
---
+++ src/ProductWriter.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
+public class ProductWriter {
+
+    public static void main(String[] args) {
+
+        /*
+        000001, Pipeweed, Long Bottom Leaf, 600.0
+        000002, Lembas, Elven Wayfare Bread, 200.0
+        000003, Wine, Woodland Elf Wine, 400.0
+        000004, Mushrooms, Farmer Took’s Finest, 125.0
+        000005, Mithril, Enchanted Dwarven Armor, 3000.0
+
+         */
+
+        ArrayList<String> products = new ArrayList<String>();
+        boolean done = false;
+        Scanner in  = new Scanner(System.in);
+
+        File workingDirectory = new File(System.getProperty("user.dir"));
+        Path file = Paths.get(workingDirectory.getPath() + "\\src\\ProductTestData.txt");
+
+        /*
+        a.	ID (a String as before in Person)
+        b.	Name (a String)
+        c.	Description (a String a short sentence)
+        d.	Cost (This is currency so it will be a Java double)
+        */
+
+        String productRec = "";
+        String ID = "";
+        String Name = "";
+        String Description = "";
+        Double Cost = 0.0;
+
+        do {
+            ID = SafeInput.getNonZeroLenString(in, "Enter your ID. [6 Digits]: ");
+            Name = SafeInput.getNonZeroLenString(in, "Enter your Name: ");
+            Description = SafeInput.getNonZeroLenString(in, "Enter your Description: ");
+            Cost = SafeInput.getDouble(in, "Enter your Cost: ");
+
+            productRec = ID + ", " + Name + ", " + Description + ", " + Cost;
+            products.add(productRec);
+
+            done = SafeInput.getYNConfirm(in, "Are you done?: ");
+
+        } while (!done);
+
+        for(String p: products) {
+            System.out.println(p);
+        }
+
+        try
+        {
+            // Typical java pattern of inherited classes
+            // we wrap a BufferedWriter around a lower level BufferedOutputStream
+            OutputStream out =
+                    new BufferedOutputStream(Files.newOutputStream(file, CREATE));
+            BufferedWriter writer =
+                    new BufferedWriter(new OutputStreamWriter(out));
+
+            // Finally can write the file LOL!
+
+            for(String rec : products)
+            {
+                writer.write(rec, 0, rec.length());  // stupid syntax for write rec
+                // 0 is where to start (1st char) the write
+                // rec. length() is how many chars to write (all)
+                writer.newLine();  // adds the new line
+
+            }
+            writer.close(); // must close the file to seal it and flush buffer
+            System.out.println("Data file written!");
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+}
